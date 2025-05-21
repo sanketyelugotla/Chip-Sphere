16c28a865b0ffc6f4dc7734d9ef4a3f77cb23e10
sanket <sanketyelugotla123@gmail.com>
2025-05-21 14:37:45 +0530

    Added pages

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className='w-full h-[4rem] flex gap-5 justify-between px-[5rem] items-center'>
+      <div className='flex gap-5'>
+        <h1>Logo</h1>
+        <h1>Chip Sphere</h1>
+      </div>
+
+      <div className='flex gap-7'>
+        <Link href='/'>Home</Link>
+        <Link href='/quizzes'>Quizzes</Link>
+        <Link href='/resources'>Resources</Link>
+        <Link href='/blogs'>Blogs</Link>
+        <Link href='/projects'>Projects</Link>
+        <Link href='/about'>About</Link>
+      </div>
+
+      <div className='flex gap-5'>
+        <h1>Mode</h1>
+        <h1>Profile</h1>
+      </div>
+    </nav>
+  )
+}

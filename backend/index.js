374065ef674443439c2456aa6267f795ddc1295d
sanket <sanketyelugotla123@gmail.com>
2025-05-22 08:13:11 +0530

    Added folders and config in backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const connectDB = require('./config/db')
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 
+// CORS
+const corsOptions = {
+    origin: ['http://localhost:3000/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// Connect to Database
+connectDB()
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World' })
+    res.status(200).json({ message: 'Chip sphere is running' })
 })
 
 
-app.listen(3000, () => {
-    console.log("listinig on port 3000")
-})
+// Port configuration and start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
